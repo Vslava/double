2afd6e215ae6ec84e0583a4c04386ea3061d92b6
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-02 12:08:51 +0700

    make args processing

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const context = require('context');
+
+// eslint-disable-next-line no-unused-expressions
+require('yargs')
+  .command({
+    command: 'collect <dirpath>',
+    desc: "Collect all files' information in the dirpath directory",
+    builder: (yargs) => {
+      yargs.positional('dirname', {
+        describe: 'A directory where the files are placed',
+        type: 'string',
+      });
+    },
+    handler: (argv) => context.services.collectFiles(argv.dirpath),
+  })
+  .scriptName('doubler')
+  .strict()
+  .demandCommand(1, 'You need at least one command before moving on')
+  .recommendCommands()
+  .help()
+  .argv;
