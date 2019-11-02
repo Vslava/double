a2ab7fe92901d69d7333dd145d529c7ab4c7cc38
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-02 10:26:53 +0700

    make eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "airbnb-base",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+  },
+  "env": {
+    "node": true,
+  },
+  "plugins": [
+  ],
+  "rules": {
+  },
+};
