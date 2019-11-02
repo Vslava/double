89fd544d51c360a683d954846f0648e7ef25fc8c
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-02 14:50:32 +0700

    make processDirectory service

---
+++ src/services/processDirectory.js
@@ -0,0 +1,25 @@
+const async = require('async');
+const fs = require('fs');
+const path = require('path');
+
+async function processDirectory({ dirpath, fileProcessor }) {
+  const dirEntry = await fs.promises.opendir(dirpath);
+
+  return async.each(dirEntry, async (dirItem) => {
+    const nextDirItemPath = path.resolve(path.join(
+      dirpath,
+      dirItem.name,
+    ));
+
+    if (dirItem.isDirectory()) {
+      return processDirectory({
+        dirpath: nextDirItemPath,
+        fileProcessor,
+      });
+    }
+
+    return fileProcessor && fileProcessor(nextDirItemPath);
+  });
+}
+
+module.exports = processDirectory;
