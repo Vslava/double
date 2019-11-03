668ac27f6308561ff4d9baac32e2457df38693cd
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-04 00:50:05 +0700

    make reading the beginning of a file

--- src/services/collectFiles.js
+++ src/services/collectFiles.js
@@ -1,6 +1,22 @@
 const async = require('async');
+const fs = require('fs');
+const util = require('util');
 const context = require('context');
 
+const fsOpen = util.promisify(fs.open);
+const fsRead = util.promisify(fs.read);
+
+const START_FILE_BYTES_COUNT = 2000000;
+
+async function readFileBeginning(filePath) {
+  const fid = await fsOpen(filePath);
+
+  const buf = Buffer.alloc(START_FILE_BYTES_COUNT);
+
+  const { buffer } = await fsRead(fid, buf, 0, START_FILE_BYTES_COUNT, 0);
+
+  return buffer;
+}
 async function fileProcessor(filePath) {
   // TODO implement file processing
 }
