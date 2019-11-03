b3fc98957c00155e0c5024fc6ba0da0b0504362c
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-04 01:22:19 +0700

    modify files fields in the migration

--- db/migrations/20191102013039_create_files_table.js
+++ db/migrations/20191102013039_create_files_table.js
@@ -1,12 +1,12 @@
 exports.up = (knex) => (
   knex.schema.createTable('files', (t) => {
     t.increments('id').primary();
-    t.string('name').notNullable();
-    t.string('md5').notNullable();
+    t.string('filepath').notNullable();
+    t.string('sign').notNullable();
 
     t.timestamps(true, true);
 
-    t.unique('md5');
+    t.unique('sign');
   })
 );
 
