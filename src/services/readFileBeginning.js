e05b8a3253e1af0563d7adad591d89cf08586e10
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-04 10:35:52 +0700

    separate readFileBeginning from collectFiles

---
+++ src/services/readFileBeginning.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const util = require('util');
+
+const fsOpen = util.promisify(fs.open);
+const fsRead = util.promisify(fs.read);
+
+const START_FILE_BYTES_COUNT = 2000000;
+
+module.exports = async (filePath) => {
+  const fid = await fsOpen(filePath);
+
+  const buf = Buffer.alloc(START_FILE_BYTES_COUNT);
+
+  const { buffer } = await fsRead(fid, buf, 0, START_FILE_BYTES_COUNT, 0);
+
+  return buffer;
+};
