8ab8ce8ba1dc91dc8fc4958ac133f5025d529921
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-04 20:43:32 +0700

    place _knex, _bookshelf into context.models

--- src/models/index.js
+++ src/models/index.js
@@ -1,11 +1,31 @@
 const _ = require('lodash');
+const knexModule = require('knex');
+const bookshelfModule = require('bookshelf');
+
 const moduleExports = require('requireindex')(__dirname);
 
 module.exports = (databaseConfig) => {
-  const knex = require('knex')(databaseConfig);
-  const bookshelf = require('bookshelf')(knex);
+  const knex = knexModule(databaseConfig);
+  const bookshelf = bookshelfModule(knex);
 
-  return _.transform(moduleExports, (result, __, modelName) => {
+  const loadedModels = _.transform(moduleExports, (result, __, modelName) => {
     result[modelName] = moduleExports[modelName](bookshelf);
   }, {});
+
+  const result = {
+    ...loadedModels,
+    _knex: knex,
+    _bookshelf: bookshelf,
+  };
+
+  Object.defineProperties(result, {
+    _knex: {
+      enumerable: false,
+    },
+    _bookshelf: {
+      enumerable: false,
+    },
+  });
+
+  return result;
 };
