9fdfb459414738d500bedc88517a9b554e39e56d
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-24 22:23:52 +0700

    refactor handlers and builders for yargs

--- src/index.js
+++ src/index.js
@@ -1,54 +1,53 @@
 const yargs = require('yargs');
 const context = require('context');
 
+async function defaultHandler(handler) {
+  await handler();
+
+  process.exit(0);
+}
+
+const commonOptions = (_yargs) => {
+  _yargs.positional('dirname', {
+    describe: 'A directory where the files are placed',
+    type: 'string',
+  });
+  _yargs.option('only-images', {
+    describe: 'Only images will be processed',
+    type: 'boolean',
+  });
+};
+
 // eslint-disable-next-line no-unused-expressions
 yargs
   .command({
     command: 'collect [--only-images] <dirpath>',
     desc: 'Collect information about all files in the dirpath directory',
-    builder: (_yargs) => {
-      _yargs.positional('dirname', {
-        describe: 'A directory where the files are placed',
-        type: 'string',
-      });
-      _yargs.option('only-images', {
-        describe: 'Only images will be processed',
-        type: 'boolean',
-      });
-    },
+    builder: commonOptions,
     handler: async (argv) => {
-      await context().services.collectFiles({
-        onlyImages: !!argv['only-images'],
-        dirpaths: [
-          argv.dirpath,
-        ],
-      });
-
-      process.exit(0);
+      await defaultHandler(() => (
+        context().services.collectFiles({
+          onlyImages: !!argv['only-images'],
+          dirpaths: [
+            argv.dirpath,
+          ],
+        })
+      ), argv);
     },
   })
   .command({
     command: 'find [--only-images] <dirpath>',
     desc: 'Find doubles in the db for all files in the dirpath directory',
-    builder: (_yargs) => {
-      _yargs.positional('dirname', {
-        describe: 'A directory where the files are placed',
-        type: 'string',
-      });
-      _yargs.option('only-images', {
-        describe: 'Only images will be processed',
-        type: 'boolean',
-      });
-    },
+    builder: commonOptions,
     handler: async (argv) => {
-      await context().services.findDoubles({
-        onlyImages: !!argv['only-images'],
-        dirpaths: [
-          argv.dirpath,
-        ],
-      });
-
-      process.exit(0);
+      await defaultHandler(() => (
+        context().services.findDoubles({
+          onlyImages: !!argv['only-images'],
+          dirpaths: [
+            argv.dirpath,
+          ],
+        })
+      ), argv);
     },
   })
   .scriptName('doubler')
