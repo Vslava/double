dadad89a8e08dbc72283520b90d03a44dc597c26
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-11-26 23:59:36 +0700

    calculate project main directory in init-dotenv

--- config/init-dotenv.js
+++ config/init-dotenv.js
@@ -2,9 +2,11 @@ const fs = require('fs');
 const path = require('path');
 const dotenv = require('dotenv');
 
+const projectDir = path.resolve(__dirname, '..');
+
 if (process.env.NODE_ENV !== 'production') {
-  const fullEnvPath = path.resolve(`.env.${process.env.NODE_ENV}`);
-  const shortEnvPath = path.resolve('.env');
+  const fullEnvPath = path.resolve(projectDir, `.env.${process.env.NODE_ENV}`);
+  const shortEnvPath = path.resolve(projectDir, '.env');
 
   if (fs.existsSync(fullEnvPath)) {
     dotenv.config({ path: fullEnvPath });
