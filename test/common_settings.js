de8bd269bb04177b6bf3201d898333909d62a334
Viacheslav Voytovich <voytovich.slava@gmail.com>
2019-12-15 13:05:51 +0700

    settings for tests

---
+++ test/common_settings.js
@@ -0,0 +1,26 @@
+/* eslint-disable jest/require-top-level-describe */
+/* eslint-disable jest/no-hooks */
+const path = require('path');
+const async = require('async');
+const sinon = require('sinon');
+const context = require('context');
+
+global.FIXTURE_DIR = path.resolve('test/fixtures');
+
+async function clearModelTables() {
+  const ctx = context();
+  const { _knex } = ctx.models;
+
+  const modelNames = Object.keys(ctx.models);
+  const modelTableNames = modelNames.map((modelName) => ctx.models[modelName].prototype.tableName);
+
+  await async.eachSeries(modelTableNames, async (modelTableName) => (
+    _knex(modelTableName).delete()
+  ));
+}
+
+afterEach(async function () {
+  sinon.restore();
+
+  await clearModelTables();
+});
